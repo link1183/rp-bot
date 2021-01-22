9bbe1082203f932ec4b654592a5305b6cca9c587
link1183 <45996502+link1183@users.noreply.github.com>
2021-01-22 13:34:57 +0100

    Create start.py

---
+++ cogs/start.py
@@ -0,0 +1,18 @@
+from discord.ext import commands
+from cogs.utils.utilitiesBot import get_client, get_config
+
+
+class StartCog(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+    
+    self.client = get_client()
+    self.config = get_config()
+    
+    embed = discord.Embed(title=f'Activity changed to {config['playing']}', description=discord.Embed.Empty, colour=discord.Color.dark_blue())
+    await ctx.send(embed=embed)
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=config['starting']))
+
+
+def setup(client):
+    cliend.add_cog(client)
