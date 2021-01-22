bd579674e0814c9f0ac2b66fa6319a7107ec89c4
link1183 <45996502+link1183@users.noreply.github.com>
2021-01-22 13:40:11 +0100

    Create stop.py

---
+++ cogs/stop.py
@@ -0,0 +1,23 @@
+from discord.ext import commands
+from discord import Embed, Activity
+from cogs.utils.utilitiesBot import get_client, get_config
+
+
+class StopCog(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+    
+    
+    client = get_client()
+    config = get_config()
+    
+    
+    @client.command(name='stop', description="Change l'activité du bot")
+    async def stop(self, ctx):
+        embed = Embed(title=f'Activity changed to {config['waiting']}', description=Embed.Empty, colour=discord.Color.dark_blue())
+        await ctx.send(embed=embed)
+        await client.change_presence(activity=discord.Activity(type=ActivityType.playing, name=config['waiting']))
+
+
+def setup(client):
+    cliend.add_cog(client)
