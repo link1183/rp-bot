6a4a9731d0c82d323fb27c16f45ad2a10d69fe67
link1183 <45996502+link1183@users.noreply.github.com>
2021-01-30 14:10:09 +0100

    Create perle.py

---
+++ cogs/perle.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+from cogs.utils.utilitiesBot import get_client
+
+
+class PerleCog(commands.Cog, name="Commande perle"):
+
+    def __init__(self, client):
+        self.client = client
+
+    client = get_client()
+
+    @client.command()
+    async def perle(self, ctx, member: discord.Member,* , sentence=None):
+        await ctx.message.delete()
+        channel = self.client.get_channel(774362633364963398)
+        embed = discord.Embed(title=f'"{sentence}"', colour=discord.Color.red())
+        embed.set_footer(text=f'Par {member.name}', icon_url=member.avatar_url)
+        await channel.send(embed=embed)
+
+
+def setup(client):
+    client.add_cog(PerleCog(client))
