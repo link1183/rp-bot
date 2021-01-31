fa933c4c51e73a2f0ac7238abe16ecac4d5384a3
link1183 <45996502+link1183@users.noreply.github.com>
2021-01-31 18:21:25 +0100

    Update spam.py

--- cogs/spam.py
+++ cogs/spam.py
@@ -1,32 +1,41 @@
 # made by : https://github.com/jubnl
 
-import discord
 from discord.ext import commands
 from discord.ext.commands.core import cooldown
 import re
 
 
-class SpamCog(commands.Cog, name="Commande +spam [@member]"):
+class SpamCog(commands.Cog, name="Commande !spam [@member]"):
 
     def __init__(self, client):
         self.client = client
 
-    @commands.has_permissions(administrator=True)
-    @commands.command(name="spam",description=" [@member] [nombre de spam] (message à spam) :\nspam un membre")
-    async def spam(self, ctx, member: discord.Member, nombre=10, *, message=None):
+    # obtention du client
 
+    @cooldown(1,180)
+    @commands.command(name="spam",description=" [@member] [nombre de spam] (message à spam) :\nspam un membre")
+    async def spam(self,ctx,member=None,nombre=None,*,message=None):
         await ctx.message.delete()
 
-        if isinstance(member, discord.Member):
-            pass
-
+        if member is not None:
+            try:
+                victim = int(re.sub(r'\D','',member))
+            except Exception:
+                return await ctx.send("Veuillez specifier un membre du discord [@member]",delete_after=5)
+            else:
+                
+                victim = self.client.get_user(victim)
+                if victim is None:
+                    return await ctx.send('L\'utilisateur a desactivé les messages privés')
+                    
         else:
             return await ctx.send("Veuillez specifier un membre du discord [@member]",delete_after=5)
 
 
         try:
-            nombre = int(nombre)
-
+            nombre=int(nombre)
+            if nombre>15:
+                nombre=15
         except ValueError:
             return await ctx.send("Veuillez entrer un nombre valide [nombre]",delete_after=5)
 
@@ -35,14 +44,13 @@ class SpamCog(commands.Cog, name="Commande +spam [@member]"):
             message = "spam"
 
         try:
-            await member.send(f"de la part de {ctx.author.display_name} :")
-
+            await victim.send(f"de la part de {ctx.author.display_name} :")
             for _ in range(nombre):
-                await member.send(message)
-
+                await victim.send(message)
         except Exception:
-            await ctx.send(f"{member.display_name} a bloqué le bot.",delete_after=5)
+            await ctx.send(f"{victim.display_name} a bloqué le bot.",delete_after=5)
 
 
 def setup(client):
     client.add_cog(SpamCog(client))
+    
