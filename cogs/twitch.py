58f8b4a5e79a629eb6353d7caff94627cf0428f6
link1183 <45996502+link1183@users.noreply.github.com>
2021-03-24 17:42:17 +0100

    Update twitch.py

--- cogs/twitch.py
+++ cogs/twitch.py
@@ -1,22 +1,46 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
+
+import requests
 
 
 class TwitchCog(commands.Cog):
+
     def __init__(self, client):
         self.client = client
+        self.is_live.start()
+    
+
+    @tasks.loop(minutes=5)
+    async def is_live(self):
+        URL = 'https://api.twitch.tv/helix/streams?user_login=link1183_'
+        auth_URL = 'https://id.twitch.tv/oauth2/token'
+        client_ID = 'r2jeo5utf9epj2xr85w7how2gw4ey2'
+        secret  = '1kxrnxpzhyki2v293opfshrg1n7btt'
+
+        aut_params = {'client_id': client_ID,
+             'client_secret': secret,
+             'grant_type': 'client_credentials'
+        }
+
+
+
+        AutCall = requests.post(url=auth_URL, params=aut_params) 
+        access_token = AutCall.json()['access_token']
+
+        head = {
+            'Client-ID' : client_ID,
+            'Authorization' :  "Bearer " + access_token
+        }
+
+        r = requests.get(URL, headers = head).json()['data']
+        channel = self.client.get_channel(823957275151564810)
+        await channel.send(r)
 
 
     @commands.command()
     async def ttv(self, ctx):
-        await ctx.send(ctx.message.author.desktop_status)
-
-
-    @commands.Cog.listener(name='on_member_update')
-    async def on_member_update(self, before, after):
-        channel = self.client.get_channel(823957275151564810)
-        await channel.send(before)
-        await channel.send(after)
+        await ctx.send('test')
     
 
 def setup(client):
