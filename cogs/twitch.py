78895ada58a2647c2bd950aa2abbce12d91d40f5
link1183 <45996502+link1183@users.noreply.github.com>
2021-03-24 21:36:50 +0100

    Update twitch.py

--- cogs/twitch.py
+++ cogs/twitch.py
@@ -1,17 +1,19 @@
 from discord.ext import commands, tasks
 import requests
 from cogs.utils.utilitiesBot import get_config
+from discord.utils import get
 
 
 class TwitchCog(commands.Cog, name='auto live message'):
 
     def __init__(self, client):
         self.client = client
+        channel = self.client.get_channel(823957275151564810)
         self.config = get_config()
         self.is_live.start()
     
 
-    @tasks.loop(minutes=5)
+    @tasks.loop(minutes=60.0)
     async def is_live(self):
         URL = 'https://api.twitch.tv/helix/streams?user_login=link1183_'
         auth_URL = 'https://id.twitch.tv/oauth2/token'
@@ -32,8 +34,9 @@ class TwitchCog(commands.Cog, name='auto live message'):
             'Authorization' :  "Bearer " + access_token
         }
 
-        r = requests.get(URL, headers = head).json()['data']
+        r = requests.get(URL, headers=head).json()['data']
         channel = self.client.get_channel(823957275151564810)
+        print(channel)
         await channel.send(r)
     
 
