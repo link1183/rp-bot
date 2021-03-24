cab8a5744e5c996de28e7a2be7db8120b1dbf291
link1183 <45996502+link1183@users.noreply.github.com>
2021-03-24 18:06:27 +0100

    updates

--- cogs/twitch.py
+++ cogs/twitch.py
@@ -1,13 +1,13 @@
-import discord
 from discord.ext import commands, tasks
-
 import requests
+from cogs.utils.utilitiesBot import get_config
 
 
-class TwitchCog(commands.Cog):
+class TwitchCog(commands.Cog, name='auto live message'):
 
     def __init__(self, client):
         self.client = client
+        self.config = get_config()
         self.is_live.start()
     
 
@@ -15,8 +15,8 @@ class TwitchCog(commands.Cog):
     async def is_live(self):
         URL = 'https://api.twitch.tv/helix/streams?user_login=link1183_'
         auth_URL = 'https://id.twitch.tv/oauth2/token'
-        client_ID = 'r2jeo5utf9epj2xr85w7how2gw4ey2'
-        secret  = '1kxrnxpzhyki2v293opfshrg1n7btt'
+        client_ID = self.config["twitch_id"]
+        secret  = self.config["twitch_secret"]
 
         aut_params = {'client_id': client_ID,
              'client_secret': secret,
@@ -24,9 +24,8 @@ class TwitchCog(commands.Cog):
         }
 
 
-
-        AutCall = requests.post(url=auth_URL, params=aut_params) 
-        access_token = AutCall.json()['access_token']
+        aut_call = requests.post(url=auth_URL, params=aut_params) 
+        access_token = aut_call.json()['access_token']
 
         head = {
             'Client-ID' : client_ID,
@@ -36,11 +35,6 @@ class TwitchCog(commands.Cog):
         r = requests.get(URL, headers = head).json()['data']
         channel = self.client.get_channel(823957275151564810)
         await channel.send(r)
-
-
-    @commands.command()
-    async def ttv(self, ctx):
-        await ctx.send('test')
     
 
 def setup(client):
