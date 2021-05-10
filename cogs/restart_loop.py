d7dcc28df5cf9d88046ae9247f0a5b1c7c0d0648
link1183 <45996502+link1183@users.noreply.github.com>
2021-05-10 18:21:19 +0200

    Update restart_loop.py

--- cogs/restart_loop.py
+++ cogs/restart_loop.py
@@ -6,12 +6,21 @@ import sys
 class RestartLoopCog(commands.Cog):
     def __init__(self, client):
         self.client = client
+        self.status = 0
         self.loop.start()
 
     
     @tasks.loop(hours=24.0)
-    async def loop(self, ctx):
-        sys.exit(0)
+    async def loop(self):
+        print('24 hours loop is starting')
+        if self.status != 0:
+            sys.exit(0)
+        self.status += 1
+
+
+    @loop.before_loop
+    async def before_loop(self):
+        await self.client.wait_until_ready()
 
 
 def setup(client):
